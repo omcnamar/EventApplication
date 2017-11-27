89edc265d17537fe2502f0f29bd5866bdf8a1c36
Oleg McNamara <omcnamar@students.kennesaw.edu>
2017-11-27 10:19:46 -0500

    added finger print authentication

---
+++ app/src/test/java/com/olegsagenadatrytwo/eventapplication/view/mainactivity/MainActivityPresenterTest.java
@@ -0,0 +1,13 @@
+package com.olegsagenadatrytwo.eventapplication.view.mainactivity;
+
+import org.junit.Test;
+
+/**
+ * Created by omcna on 11/26/2017.
+ */
+public class MainActivityPresenterTest {
+    @Test
+    public void getEvents() throws Exception {
+    }
+
+}
